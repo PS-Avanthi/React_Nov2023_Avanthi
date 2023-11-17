b3cae2b3cf6712509db2f198323e0f29b1a9d4fd
PS-Avanthi <124856237+PS-Avanthi@users.noreply.github.com>
2023-11-17 10:28:42 +0530

    DAY5

---
+++ day5/reactapp1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Lifecycle from './components/Lifecycle';
+import Funlifecycle from './components/Funlifecycle';
+function App() {
+  return (
+    <div className="App">
+      <Lifecycle/>
+      <Funlifecycle/>
+    </div>
+  );
+}
+export default App;
