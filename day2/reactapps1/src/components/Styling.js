3eab001cd29b057ff0af8e9e2627e23122d85e85
PS-Avanthi <124856237+PS-Avanthi@users.noreply.github.com>
2023-11-17 10:01:17 +0530

    DAY2

---
+++ day2/reactapps1/src/components/Styling.js
@@ -0,0 +1,17 @@
+import React from "react";
+const Styling = () =>
+{
+    const pstyle=
+    {
+        backgroundColor: "lightblue",
+        color: "darkblue",
+    };
+    return (
+    
+        <div>
+            <h1 style={{color:"green"}}>Inline style in JSX Example</h1>
+            <p style={pstyle}>This is a paragraph with inline styles app</p>
+        </div>
+    );
+};
+export default Styling;
---
+++ day2/reactapps2/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import Helloworld from "./components/Helloworld";
+function App()
+{
+  return(
+    <div className="App">
+    <Helloworld />
+    </div>
+  );
+}
